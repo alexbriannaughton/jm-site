59ca9b9b35e7cc1a3ad501114e622ae589bd2877
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-03-15 19:38:24 -0700

    click outside of modal for mobile fix

--- callMeModal.js
+++ callMeModal.js
@@ -18,7 +18,7 @@ span.onclick = function () {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.addEventListener("onmousedown", function (event) {
+document.addEventListener("onmousedown", function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
