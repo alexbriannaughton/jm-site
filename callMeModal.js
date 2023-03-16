08dcda2615b171ab74a1bd1f458f05867073b2d0
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-03-15 19:53:15 -0700

    rollback

--- callMeModal.js
+++ callMeModal.js
@@ -8,24 +8,33 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function () {
+btn.onclick = function() {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+span.onclick = function() {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.addEventListener("click", function (event) {
+window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-});
+}
 
-document.addEventListener("touchstart", function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-});
+
+// with the below code, social buttons stop working after the first time they're clicked. but with above code mobile cannot close modal on outside click
+
+// window.addEventListener("click", function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// });
+
+// document.addEventListener("touchstart", function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// });
